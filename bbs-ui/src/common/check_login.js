8e2344b8e6e5c964df74b74ad395bbd59206224e
maquan0927 <maquan0927@126.com>
2020-04-16 10:24:20 +0800

    增加登陆提示

--- bbs-ui/src/common/check_login.js
+++ bbs-ui/src/common/check_login.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import VueCookie from 'vue-cookie'
-Vue.use(VueCookie)
 import router from '../router/router.js'
+import { MessageBox } from 'element-ui'
 
-Vue.prototype.check_login = function(){
+Vue.use(VueCookie)
+
+Vue.prototype.check_login = function(url="/ui"){
   /* 
   验证用户是否登陆
   */
   if(Vue.cookie.get('access_token')){
     return true
   }else{
-    router.push('/ui/login')
+    MessageBox.confirm('该操作需要登陆，是否现在登陆？', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+      title: '登陆提示'
+    }).then(() => {
+      router.push("/ui/login?next=" + url)
+    }).catch(() => {
+      return
+    });
   }
 }
